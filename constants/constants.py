24bc96da49c91904bd615f11b2bc5116e29bf8e8
Rayirth Soni <rayirthsoni04@gmail.com>
2024-07-13 16:19:38 +0530

    added new files

---
+++ constants/constants.py
@@ -0,0 +1,37 @@
+"""Constants
+============
+
+This module contains constants that have been used in entire codebase
+"""
+
+# Ignore pylint warngings
+# pylint: disable=line-too-long
+
+
+class Constants:
+    """constants configurations"""
+
+    INAZUMA_ELEVEN1_WEB_FEATURES = {"level_99_stats": "1/stats_ie1"}
+
+    INANUMA_ELEVEN2_WEB_FEATURES = {
+        "connection_map": "2/inazuma_eleven_2_connection_map",
+        "moves_list": "2/waza_ie2",
+        "recuritments": "2/recruit",
+        "friendly_matches": "2/ie2_friendly_matches",
+        "level_99_stats": "2/stats_ie2",
+        "cheats": "2/inazuma-eleven-2-cheats-action-replay-codes",
+    }
+
+    INAZUMA_ELEVEN3_WEB_FEATURES = {
+        "connection_map": "3/ie3_connection_map",
+        "recuritments": "3/ie3_recruit",
+        "friendly_matches": "3/ie3_friendly_matches",
+        "level_99_stats": "3/inazuma-eleven-3-stats-players-level-99",
+    }
+
+    class Scripts:
+        """scripts constants"""
+
+        # WEB_HOME_URL = "http://watashiwa7.altervista.org/ie/index.html"
+        WEB_INAZUMA_ELEVEN_URL = "http://watashiwa7.altervista.org/ie/{}.htm"
+
---
+++ scripts/data_scrape.py
@@ -0,0 +1,21 @@
+"""Data Scrape
+==============
+
+This module contains code for scraping data from the web.
+"""
+
+# Ignore pylint warnings
+# pylint: disable=line-too-long
+import os
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+from constants.constants import Constants
+
+os.chdir("..")
+
+
+
+
+
